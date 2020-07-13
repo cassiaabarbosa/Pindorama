815e323cf2e5b31bd4f2bffe3a5b3cf09b7d87a7
Cassia <cassia.a.barbosa@gmail.com>
2020-07-13 19:17:22 -0300

    user defaults

---
+++ appTV/appTV/QuestionScreen/Model/StateManager.swift
@@ -0,0 +1,9 @@
+//
+//  StateManager.swift
+//  appTV
+//
+//  Created by Cassia Aparecida Barbosa on 13/07/20.
+//  Copyright © 2020 com.CassiaABarbosa. All rights reserved.
+//
+
+import Foundation
